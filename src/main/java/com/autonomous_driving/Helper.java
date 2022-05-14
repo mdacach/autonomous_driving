712ff9f8cf4f2c36713aa416a47b73e8fcde87fe
Matheus Cardoso <dacachmatheus@gmail.com>
2022-05-13 21:28:36 -0300

    Refactor code from tab2 to tab4

--- src/main/java/com/autonomous_driving/Helper.java
+++ src/main/java/com/autonomous_driving/Helper.java
@@ -1,49 +1,49 @@
 package com.autonomous_driving;
 
-import javafx.scene.image.ImageView;
 import javafx.application.Platform;
+import javafx.scene.image.ImageView;
 
 /**
-* Helper class with various utility functions.
-* */
+ * Helper class with various utility functions.
+ */
 abstract class Helper {
-  /**
-  * Wait wrapper.
-  * */
-  static void waitTime(long milis) {
-    try {
-      Thread.sleep(milis);
-    } catch (Exception e) {
-      e.printStackTrace();
+    /**
+     * Wait wrapper.
+     */
+    static void waitTime(long milis) {
+        try {
+            Thread.sleep(milis);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-  }
 
-  /**
-  * Check if two positions are the same.
-  * */
-  static public boolean same(Position lhs, Position rhs) {
-    return lhs.x == rhs.x && lhs.y == rhs.y;
-  }
+    /**
+     * Check if two positions are the same.
+     */
+    static public boolean same(Position lhs, Position rhs) {
+        return lhs.x == rhs.x && lhs.y == rhs.y;
+    }
 
-  /**
-  * Adds image view to frame, when possible.
-  * */
-  static public void addImageView(ImageView imageView, Position position) {
-    Platform.runLater(() -> {
-      Globals.root.getChildren().add(imageView);
-      imageView.setX(position.x);
-      imageView.setY(position.y);
-    });
-  }
+    /**
+     * Adds image view to frame, when possible.
+     */
+    static public void addImageView(ImageView imageView, Position position) {
+        Platform.runLater(() -> {
+            Globals.root.getChildren().add(imageView);
+            imageView.setX(position.x);
+            imageView.setY(position.y);
+        });
+    }
 
-  /**
-  * Updates an image view on frame, when possible.
-  * */
-  static public void updateImageView(ImageView imageView, Position position) {
-    Platform.runLater(() -> {
-      imageView.setX(position.x);
-      imageView.setY(position.y);
-    });
-  }
+    /**
+     * Updates an image view on frame, when possible.
+     */
+    static public void updateImageView(ImageView imageView, Position position) {
+        Platform.runLater(() -> {
+            imageView.setX(position.x);
+            imageView.setY(position.y);
+        });
+    }
 
 }
--- src/main/java/com/autonomous_driving/Position.java
+++ src/main/java/com/autonomous_driving/Position.java
@@ -1,42 +1,44 @@
 package com.autonomous_driving;
 
 /**
-* Helper class representing a position on the interface.
-*
-* All positions are specified in image "all_points.png"
-* on images folder.*/
+ * Helper class representing a position on the interface.
+ * <p>
+ * All positions are specified in image "all_points.png"
+ * on images folder.
+ */
 class Position {
-  int x, y;
-  public Position(int x, int y) {
-    this.x = x - Globals.OFFSET; // offset for image
-    this.y = y - Globals.OFFSET;
-  }
+    int x, y;
 
-  // debug purposes
-  public String toString() {
-    return "(" + x + ", " + y + ")";
-  }
+    public Position(int x, int y) {
+        this.x = x - Globals.OFFSET; // offset for image
+        this.y = y - Globals.OFFSET;
+    }
 
-  // to be used inside Car
-  public void left() {
-    this.x--;
-  }
+    // debug purposes
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
 
-  public void up() {
-    this.y--;
-  }
+    // to be used inside Car
+    public void left() {
+        this.x--;
+    }
 
-  public void right() {
-    this.x++;
-  }
+    public void up() {
+        this.y--;
+    }
 
-  public void down() {
-    this.y++;
-  }
+    public void right() {
+        this.x++;
+    }
 
-  // copy this position
-  // as java is weird (not really pass by value nor reference)
-  public Position copy() {
-    return new Position(x + Globals.OFFSET, y + Globals.OFFSET);
-  }
+    public void down() {
+        this.y++;
+    }
+
+    // copy this position
+    // as java is weird (not really pass by value nor reference)
+    public Position copy() {
+        return new Position(x + Globals.OFFSET, y + Globals.OFFSET);
+    }
 }
